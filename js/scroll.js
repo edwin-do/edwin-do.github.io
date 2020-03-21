c7149e72e05a87c3dc3985cccc51ee9677e53cde
edwin-do <doedwin123@gmail.com>
2020-03-20 22:17:18 -0400

    update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,18 @@
 // init controller
 var controller = new ScrollMagic.Controller();
 
+var intro = new ScrollMagic.Scene({
+    duration: 1000, // the scene should last for a scroll distance of 100px
+    triggerElement: "#top",
+    offset: 300 // start this scene after scrolling for 50px
+})
+
+.setPin('#top') // pins the element for the the scene's duration
+.addTo(controller); // assign the scene to the controller
+
 // create a scene
 var edu = new ScrollMagic.Scene({
-    duration: 350, // the scene should last for a scroll distance of 100px
+    duration: 500, // the scene should last for a scroll distance of 100px
     triggerElement: "#edu",
     offset: 250 // start this scene after scrolling for 50px
 })
@@ -12,7 +21,7 @@ var edu = new ScrollMagic.Scene({
 .addTo(controller); // assign the scene to the controller
 
 var learning = new ScrollMagic.Scene({
-    duration: 350, // the scene should last for a scroll distance of 100px
+    duration: 500, // the scene should last for a scroll distance of 100px
     triggerElement: "#learn",
     offset: 250 // start this scene after scrolling for 50px
 })
