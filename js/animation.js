86e594e7053b3087618e123bc53278d54987db57
edwin-do <doedwin123@gmail.com>
2020-05-07 13:06:58 -0400

    edit intro animation

--- js/animation.js
+++ js/animation.js
@@ -2,7 +2,7 @@ var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
-    this.period = parseInt(period, 6) || 3000;
+    this.period = parseInt(period, 2) || 2000;
     this.txt = '';
     this.tick();
     this.isDeleting = true;
