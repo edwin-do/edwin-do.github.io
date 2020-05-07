948605c1745b4af21d562d017eac2a3306736fc5
edwin-do <doedwin123@gmail.com>
2020-05-07 10:03:30 -0400

    add scroll animation

--- js/scroll.js
+++ js/scroll.js
@@ -1,78 +1,34 @@
 // init controller
 var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
 
-//edu
+    //About
     new ScrollMagic.Scene({
-        triggerElement: "#trigger-edu-img",
-        triggerHook: 1.0, 
-        duration: "140%", 
-        offset: 60 
-    })
-    .setClassToggle("#reveal-edu-img", "visible") 
-    .addTo(controller);
-
-    new ScrollMagic.Scene({
-        triggerElement: "#trigger-edu-txt",
-        triggerHook: 1.0,
-        duration: "120%", 
-        offset: 10 
-    })
-    .setClassToggle("#reveal-edu-txt", "visible")
-    .addTo(controller);
-
-//learn
-    new ScrollMagic.Scene({
-        triggerElement: "#trigger-learn-img",
+        triggerElement: "#trigger-about",
         triggerHook: 1.0,
         duration: "140%",
-        offset: 60
+        offset: 100
     })
-    .setClassToggle("#reveal-learn-img", "visible")
+    .setClassToggle("#reveal-about", "visible")
     .addTo(controller);
 
+    //Experience
     new ScrollMagic.Scene({
-        triggerElement: "#trigger-learn-txt",
-        triggerHook: 1.0,
-        duration: "120%",
-        offset: 10
-    })
-    .setClassToggle("#reveal-learn-txt", "visible")
-    .addTo(controller);
-
-//Music
-    new ScrollMagic.Scene({
-        triggerElement: "#trigger-music-img",
+        triggerElement: "#trigger-exp",
         triggerHook: 1.0,
         duration: "140%",
-        offset: 60
+        offset: 100
     })
-    .setClassToggle("#reveal-music-img", "visible")
+    .setClassToggle("#reveal-exp", "visible")
     .addTo(controller);
 
+    //skills
     new ScrollMagic.Scene({
-        triggerElement: "#trigger-music-txt",
-        triggerHook: 1.0,
-        duration: "120%",
-        offset: 10
-    })
-    .setClassToggle("#reveal-music-txt", "visible")
-    .addTo(controller);
-
-//Food
-    new ScrollMagic.Scene({
-        triggerElement: "#trigger-food-img",
+        triggerElement: "#trigger-skills",
         triggerHook: 1.0,
         duration: "140%",
-        offset: 60
+        offset: 100
     })
-    .setClassToggle("#reveal-food-img", "visible")
+    .setClassToggle("#reveal-skills", "visible")
     .addTo(controller);
 
-    new ScrollMagic.Scene({
-        triggerElement: "#trigger-food-txt",
-        triggerHook: 1.0,
-        duration: "120%",
-        offset: 10
-    })
-    .setClassToggle("#reveal-food-txt", "visible")
-    .addTo(controller);
+    
